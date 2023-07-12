bc474fe303947d0bb584779ad4d898b598a6b17f
targinu <targino26@outlook.com>
2023-07-12 15:18:09 -0300

    adiciona reprodução de som

---
+++ 01 - JS Drumkit/script.js
@@ -0,0 +1,19 @@
+//adicionar evento de pressionar tecla
+window.addEventListener("keydown", handleKeyDown);
+
+//função para reproduzir o som
+function playSound(audioElement) {
+  audioElement.currentTime = 0;
+  audioElement.play();
+}
+
+//função para tratar o evento de pressionar uma tecla
+function handleKeyDown(event) {
+  const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+
+  if (key && audio) {
+    key.classList.add("playing");
+    playSound(audio);
+  }
+}
