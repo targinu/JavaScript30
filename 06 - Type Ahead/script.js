dd687117e69100d378cf0710a937311d25d220bc
targinu <targino26@outlook.com>
2023-07-18 14:05:30 -0300

    criaçao do formulario de pesquisa + estilos

---
+++ 06 - Type Ahead/script.js
@@ -0,0 +1,3 @@
+//endpoint para o arquivo JSON contendo os estados e cidades brasileiras
+const endpoint =
+  "https://gist.githubusercontent.com/letanure/3012978/raw/6938daa8ba69bcafa89a8c719690225641e39586/estados-cidades.json";
